be2f9616ed66364015f3327f45f217501d9a5667
Jay Vilkajs <vilkajs.janis@gmail.com>
2020-08-01 23:20:50 +0300

    Added Register component, minor changes to login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { IngredientFormComponent } from './ingredient-form/ingredient-form.compo
 import { UomListComponent } from './uom-list/uom-list.component';
 import { UomFormComponent } from './uom-form/uom-form.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { LoginComponent } from './login/login.component';
     UomFormComponent,
     NaviComponent,
     LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
